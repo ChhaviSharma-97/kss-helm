af40f50e98c04b1682be227a7d5f5ca178b572d8
atishay.jain <atishay.jain@tothenew.com>
2023-06-28 21:23:16 +0530

    app-infra

--- test-infra/addon-resources/kms.tf
+++
@@ -1,7 +0,0 @@
-data "template_file" "kms" {
-  template = "${file("${path.module}/kms_policy.json.tpl")}"
-  vars = {
-    Account_ID = "${local.workspace.aws.account_id}"
-    Role_Name = "${local.workspace.kms_policy.role_name}"
-  }
-}
